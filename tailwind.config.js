56775ae116d521c6389f0040c990e9735ac73ecb
Vahid Kavazovic <vahid.kavazovic@bild-studio.net>
2021-09-22 13:10:12 +0200

    Layout changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
-  mode:'jit',
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom:
+          "0 -5px 5px -5px rgba(0, 0, 0, 0.4), 0px 5px 5px -5px rgba(0, 0, 0, 0.4)",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
